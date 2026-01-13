65781626c431dc207d78aed3fce83ae65dca3b6b
Hiroyuki Kai <fullerenec84@gmail.com>
2026-01-13 14:58:29 +0900

    Update to manifest v3. Also update webpack etc.

--- src/background.js
+++ src/background.js
@@ -23,47 +23,47 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const id = "" + Date.now()
     obj[id] = request.payload
     chrome.storage.local.set(obj)
-  }
-})
-
-chrome.storage.onChanged.addListener(() => {
-  chrome.storage.local.get(null, (items) => {
-    console.log(items)
-    const data = {
-      total: items["__total"],
-      items: [],
-    }
-
-    for (const [k, v] of Object.entries(items)) {
-      if (k.indexOf("__") != 0) {
-        data.items.push([v.price, v.id, v.max_count, v.min_count || 0])
+  } else if (req.type === "CALCULATE") {
+    chrome.storage.local.get(null, (items) => {
+      console.log(items)
+      const data = {
+        total: items["__total"],
+        items: [],
       }
-    }
-    const url = "https://979ziepbpc.execute-api.ap-northeast-1.amazonaws.com/default/linear_programming"
-    fetch(url, {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((r) => {
-        return r.json()
-      })
-      .then((result) => {
-        const data = JSON.parse(result.body)
-        console.log("Response from AWS backend", data)
-        if (data.count) {
-          const obj = {}
-          obj["__calculated"] = data.count
-          chrome.storage.local.set(obj)
-          chrome.storage.local.get(null, (items) => {
-            console.log({ items })
-          })
-        } else {
-          chrome.storage.local.remove("__calculated")
+
+      for (const [k, v] of Object.entries(items)) {
+        if (k.indexOf("__") != 0) {
+          data.items.push([v.price, v.id, v.max_count, v.min_count || 0])
         }
+      }
+      const url = "https://979ziepbpc.execute-api.ap-northeast-1.amazonaws.com/default/linear_programming"
+      fetch(url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
       })
-  })
+        .then((r) => {
+          return r.json()
+        })
+        .then((result) => {
+          const data = JSON.parse(result.body)
+          console.log("Response from AWS backend", data)
+          if (data.count) {
+            const obj = {}
+            obj["__calculated"] = data.count
+            chrome.storage.local.set(obj)
+            chrome.storage.local.get(null, (items) => {
+              console.log({ items })
+            })
+          } else {
+            chrome.storage.local.remove("__calculated")
+          }
+          sendResponse({ ok: true })
+        })
+    })
+    return true
+  }
 })
